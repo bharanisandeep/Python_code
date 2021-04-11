c203083f66eb10200991fe37ea5c2e0205f0f40e
sandeep <bharanisandeep4@gmail.com>
2021-04-11 06:04:46 -0700

    first commit

---
+++ list_intro.py
@@ -0,0 +1,17 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse-pad"
+                  ]
+
+for parts in computer_parts:
+    print(parts)
+
+print()
+print(computer_parts[2])
+
+#slicing the list
+print(computer_parts[0:3])
+print(computer_parts[1:3])
+print(computer_parts[-1])
